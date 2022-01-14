6e701940c2780838650e2754ba6eda9af5be8de5
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-14 14:59:16 +0530

    Add files via upload

---
+++ pratical2_2_c.py
@@ -0,0 +1,12 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - A python program to add an item in a tuple.
+Github link - https://github.com/Virti7702/Practical2-2-c.git
+
+'''
+t=(1,2,3,4)
+v=list(t)
+v.append(5)
+t=tuple(v)
+print(t)
